c1a24969972372b397e1dfa8dc88a8e95a67b10d
Maxim Golendukhin <golendukhin.maxim@gmail.com>
2019-05-26 15:56:23 +1000

    downloader sleeps if unable to download file

--- downloader.py
+++ downloader.py
@@ -1,7 +1,7 @@
 from ftplib import FTP
 import os
 from datetime import datetime as dt
-import glob
+import time
 
 ftp = FTP('ftp.zakupki.gov.ru')
 ftp.login(user='fz223free', passwd='fz223free')
@@ -18,14 +18,21 @@ for region in ftp.nlst():
     except:
         pass
 
-    already_downloaded_files = glob.glob(region_path + '/*')
-    already_downloaded_files = [item + '.zip' for item in already_downloaded_files if item[-3:] != 'zip']
+    already_downloaded_files = os.listdir(region_path)
+    already_downloaded_files = [item + '.zip' if item[-3:] != 'zip' else item for item in already_downloaded_files]
 
     for file in ftp.nlst():
-        file_to_download = os.path.join(region_path, file)
-        if file_to_download in already_downloaded_files:
+        if file in already_downloaded_files:
             continue
 
+        file_to_download = os.path.join(region_path, file)
         with open(file_to_download, 'wb') as f:
-            ftp.retrbinary('RETR ' + file, f.write)
+            is_downloaded = False
+            while not is_downloaded:
+                try:
+                    ftp.retrbinary('RETR ' + file, f.write)
+                    is_downloaded = True
+                except:
+                    print('was unable to download', file, '. Sleeping for 10 seconds')
+                    time.sleep(10)
         print('file', file, 'was downloaded at', dt.now())
